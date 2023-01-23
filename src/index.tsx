b3fd451f885866731d5e5bdc9582ac08ff419f16
Alex Ilin <alexander.ilyin3@gmail.com>
2023-01-23 03:04:35 +0200

    chore: ReactDom.render console error fixing

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import App from './App';
 import './index.css';
 import './scss/main.scss';
+import ReactDOM from 'react-dom/client';
 
-// import './scss/main.scss';
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
